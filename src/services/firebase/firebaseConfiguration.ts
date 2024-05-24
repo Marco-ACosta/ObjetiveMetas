01f21e72500d14530c71431318e4d4b7e1e7d7a6
ThalesOfMiletus <thalesmartinscogo@gmail.com>
2024-05-24 01:09:31 -0300

    config env

--- src/services/firebase/firebaseConfiguration.ts
+++ src/services/firebase/firebaseConfiguration.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYEujcI9Xtvhc80Btxe6I-E7DPIcNwvyI",
-    authDomain: "avaliacaodevsis.firebaseapp.com",
-    databaseURL: "https://avaliacaodevsis-default-rtdb.firebaseio.com",
-    projectId: "avaliacaodevsis",
-    storageBucket: "avaliacaodevsis.appspot.com",
-    messagingSenderId: "360624263341",
-    appId: "1:360624263341:web:45b3db402c294cee7a7624"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
